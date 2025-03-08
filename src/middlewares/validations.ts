f5ea55f78b1d9b34786b376aab9ba26c69d11f3c
Enrique Vicente <enriqueadhemar@gmail.com>
2025-03-08 12:30:43 -0400

    feat: add request data validation middleware

---
+++ src/middlewares/validations.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { body, validationResult } from 'express-validator'
+
+const validationResponse = (req: Request, res: Response, next: NextFunction) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    next();
+  } else {
+    res.status(400).json({
+      message: 'Error validating data',
+      errors: result,
+    })
+  }
+}
+
+export const validateCreateEvent = [
+  body('name').isString().withMessage('Name must be a valid string'),
+  body('date').isDate({format: 'YYYY-MM-DD'})
+  .custom(value => new Date(value) > new Date())
+  .withMessage('Date must be a future date with YYYY-MM-DD format'),
+  body('availableSeats').isInt({ gt: 0 }).withMessage('The available seats must be a positive integer'),
+  validationResponse,
+]
--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as eventController from '../controllers/events.controller'
+import { validateCreateEvent } from "../middlewares/validations";
 
 const router = Router();
 
-router.post('/', eventController.createEvent);
+router.post('/', validateCreateEvent, eventController.createEvent);
 router.get('/', eventController.getEvents);
 
 export default router;
